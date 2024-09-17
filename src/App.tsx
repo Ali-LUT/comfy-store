4f0f11a5fa465f031dfa4ad0680afafad0ccf85b
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-09-17 21:40:42 +0300

    Added ErrorElement component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import {
   Orders,
 } from "./pages";
 
+import { ErrorElement } from "./components";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,27 +25,33 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "products",
         element: <Products />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "cart",
         element: <Cart />,
+        errorElement: <ErrorElement />,
       },
-      { path: "about", element: <About /> },
+      { path: "about", element: <About />, errorElement: <ErrorElement /> },
       {
         path: "checkout",
         element: <Checkout />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "orders",
         element: <Orders />,
+        errorElement: <ErrorElement />,
       },
     ],
   },
