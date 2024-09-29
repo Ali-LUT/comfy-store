65f9d8766490e2cf29a3a1f221f049a6dc0b826f
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-09-29 23:15:27 +0300

    Added register page setup and action

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,44 @@
+import { ActionFunction, Form, Link, redirect } from "react-router-dom";
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { SubmitBtn, FormInput } from "@/components";
+import { customFetch } from "@/utils";
+import { toast } from "@/components/ui/use-toast";
+import { AxiosError } from "axios";
+
+export const action: ActionFunction = async ({ request }): Promise<null> => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  return null;
+};
+
 function Register() {
-  return <h1 className="text-4xl">Register Page</h1>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <CardTitle className="text-center">Register</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form method="post" action="">
+            <FormInput type="text" name="username" defaultValue="test" />
+            <FormInput type="email" name="email" defaultValue="test@test.com" />
+            <FormInput type="password" name="password" defaultValue="secret" />
+
+            <Button type="submit" variant="default" className="w-full mt-4">
+              Submit
+            </Button>
+
+            <p className="text-center mt-4">
+              Already a member?
+              <Button type="button" asChild variant="link">
+                <Link to="/login">Login</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
 export default Register;
